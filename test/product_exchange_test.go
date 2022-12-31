d96f43b0b8f480d7a01a786896dde5dbdff6161a
aohanhongzhi <aohanhongzhi@qq.com>
2022-12-31 12:16:33 +0800

    重新连接的机制，监听错误的机制

--- test/product_exchange_test.go
+++ test/product_exchange_test.go
@@ -20,3 +20,14 @@ func SendExchangeHxb(num int) {
 		fmt.Println(err)
 	}
 }
+
+func TestSendExchangeTopic(t *testing.T) {
+	initrabbitmq()
+	data := kelleyRabbimqPool.GetRabbitMqDataFormat("register-test-exchange", kelleyRabbimqPool.EXCHANGE_TYPE_TOPIC, "", "jt-wechat", fmt.Sprintf("这里是数据%d", 1))
+	err := instanceRPool.Push(data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	time.Sleep(3 * time.Second)
+
+}
